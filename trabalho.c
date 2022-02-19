26a72a62eacbd389330e7c9335c786c030b0a729
karimdfk <99849759+karimdfk@users.noreply.github.com>
2022-02-19 16:00:11 -0300

    add contatos do resort

--- trabalho.c
+++ trabalho.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct a_definir{
 	//inserir código aqui
 };
+
 
 void cadastro(struct a_definir T[]){
 	//inserir código aqui
@@ -17,6 +19,13 @@ void mostra1(struct a_definir T[]){
 	//inserir código aqui
 }
 
+void mostra_contatos(){
+    printf("\ninstagram: resort_instagram\n");
+    printf("gmail: resort@gmail.com\n");
+    printf("Telefone: (34)99877-0392\n");
+}
+
+
 void menu(){
 	int opcao;
 	struct a_definir P[30];
@@ -26,6 +35,7 @@ void menu(){
 		printf("\n1- Cadastrar ");
 		printf("\n2- Mostrar Todos");
 		printf("\n3- Mostrar um");
+		printf("\n7- Mostrar Contatos");
 		printf("\n9- Sair ");
 		printf("\nDigite opção: ");
 		scanf("%d", &opcao);
@@ -33,6 +43,7 @@ void menu(){
 		if(opcao == 1) cadastro(P);
 		if(opcao == 2) mostra(P);
 		if(opcao == 3) mostra1(P);
+		if(opcao == 7) mostra_contatos();
 		if(opcao == 9) return;
 	}
 }
@@ -41,3 +52,4 @@ void menu(){
 int main(){
 	menu();
 }
+
