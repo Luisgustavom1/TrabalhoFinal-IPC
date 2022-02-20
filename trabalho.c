a04fb449af8d6e1dd59a59617f05edac182f0aad
Luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-02-20 17:50:41 -0300

    Revert "V1 - Ler dados pronto"
    This reverts commit ec0b269d627f054375b3c5592245e2de020033b5.

--- trabalho.c
+++ trabalho.c
@@ -1,87 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct a_definir
-{
-	// inserir código aqui
+struct a_definir{
+	//inserir código aqui
 };
 
-struct Feedback
-{
-	char comments[100];
-	char name[60];
-	int stars;
-};
-
-void cadastro(struct a_definir T[])
-{
-	// inserir código aqui
+void cadastro(struct a_definir T[]){
+	//inserir código aqui
 }
 
-void mostra(struct a_definir T[])
-{
-	// inserir código aqui
+void mostra(struct a_definir T[]){
+	//inserir código aqui
 }
 
-void mostra1(struct a_definir T[])
-{
-	// inserir código aqui
+void mostra1(struct a_definir T[]){
+	//inserir código aqui
 }
 
-void seeAllFeedbacks()
-{
-	int c = 0;
-	struct Feedback F;
-	
-	FILE *file;
-	file = fopen("teste.txt", "r");
-
-	printf("Feedback do resort mais brabo que tem!!\n");
-	printf("==========================\n");
-	while (fscanf(file, "%s %d", F.name, &F.stars) != EOF)
-	{
-		fgets(F.comments, 100, file);
-
-		printf("%s %d estrelas:\n", F.name, F.stars);
-		printf("- %s\n", F.comments);
-		
-		c++;
-	}
-	printf("==========================");
-	
-	fclose(file);
-}
-
-void menu()
-{
+void menu(){
 	int opcao;
 	struct a_definir P[30];
 
-	while (1)
-	{
+	while(1){
 		printf("\nBem vindo ao Sistema de... ");
 		printf("\n1- Cadastrar ");
 		printf("\n2- Mostrar Todos");
 		printf("\n3- Mostrar um");
-		printf("\n6- Ver feedbacks");
 		printf("\n9- Sair ");
 		printf("\nDigite opção: ");
 		scanf("%d", &opcao);
-
-		if (opcao == 1)
-			cadastro(P);
-		if (opcao == 2)
-			mostra(P);
-		if (opcao == 3)
-			mostra1(P);
-		if (opcao == 6)
-			seeAllFeedbacks();
-		if (opcao == 9)
-			return;
+	
+		if(opcao == 1) cadastro(P);
+		if(opcao == 2) mostra(P);
+		if(opcao == 3) mostra1(P);
+		if(opcao == 9) return;
 	}
 }
 
-int main()
-{
+
+int main(){
 	menu();
 }
